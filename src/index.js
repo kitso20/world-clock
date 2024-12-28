94e1412ad292a68ac1348497cc1f9cb37149dcc0
kitso918 <motlashupingkitso@gmail.com>
2024-12-28 15:02:30 +0200

    moment for 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-alert (`hey there!`)
+function updateTime(){
+let southA = document.querySelector(`#africa`)
+let SAdate = southA.querySelector(`.date`)
+let SAtime = southA.querySelector(`.time`)
+let SAlocal = moment.tz(`Africa/Johannesburg`)
+SAdate.innerHTML = SAlocal.format(`Do MMMM YYYY`)
+SAtime.innerHTML = SAlocal.format(`h:mm:ss [<small>]A[</small>]`)
+
+let southK = document.querySelector(`#koria`)
+let SKdate = southK.querySelector(`.date`)
+let SKtime = southK.querySelector(`.time`)
+let SKlocal = moment.tz(`Asia/Seoul`)
+SKdate.innerHTML = SKlocal.format(`Do MMMM YYYY`)
+SKtime.innerHTML = SKlocal.format(`h:mm:ss [<small>]A[</small>]`)
+}
+
+setInterval(updateTime,1000)
